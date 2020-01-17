e297b679e8c5b5743a6d8ce7a51bb049a20a5b76
rootadminWalker <47111874+rootadminWalker@users.noreply.github.com>
2020-01-17 16:39:08 +0800

    First file

---
+++ Installation.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Press CTRL+C to interrupt or ENTER to continue
+read
+
+sudo apt install build-essential
+sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-devlibtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
