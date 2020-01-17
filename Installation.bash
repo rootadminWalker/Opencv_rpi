9c792d850c6bd8b99f59a5a2196584501938083f
rootadminWalker <47111874+rootadminWalker@users.noreply.github.com>
2020-01-17 16:47:04 +0800

    Update installation.bash

--- Installation.bash
+++ Installation.bash
@@ -5,3 +5,5 @@ read
 
 sudo apt install build-essential
 sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-devlibtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+sudo -H pip install opencv-python opencv-contrib-python
